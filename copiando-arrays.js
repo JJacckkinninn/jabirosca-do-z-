e3537b1a76d8466309d85b7dab6a8eb2b7fd8ef5
Jacklin <132667129+JJacckkinninn@users.noreply.github.com>
2024-04-10 08:02:10 -0300

    Update copiando-arrays.js

--- copiando-arrays.js
+++ copiando-arrays.js
@@ -33,3 +33,90 @@ const cliente = {
 };
 
 console.log(cliente)
+
+
+const cliente = {
+  nome: "Andre",
+  idade: 32,
+  cpf: "1122233345",
+  email: "andre@dominio.com",
+};
+
+console.log(
+  `O nome do cliente é ${cliente["nome"]} e essa pessoa tem ${cliente["idade"]} anos.`
+);
+
+console.log(`Os 3 primeiros digitos do CPF são ${cliente.cpf.substring(0, 3)}`);
+VM344:8 O nome do cliente é Andre e essa pessoa tem 32 anos.
+VM344:12 Os 3 primeiros digitos do CPF são 112
+undefined
+
+
+
+const cliente = {
+  nome: "Andre",
+  idade: 32,
+  cpf: "1122233345",
+  email: "andre@dominio.com",
+};
+
+console.log(
+  `O nome do cliente é ${cliente["nome"]} e essa pessoa tem ${cliente["idade"]} anos.`
+);
+
+const chaves = ["nome", "idade", "cpf", "email"];
+
+chaves.forEach((chave) => {
+  console.log(`A chave ${chave} tem valor ${cliente[chave]}`);
+});
+
+
+const pessoa = {
+  nome: "Luma",
+  profissao: "Engenheira",
+};
+
+console.log(pessoa.nome);
+
+console.log(pessoa.telefone);
+
+pessoa.telefone = "11 2223333444";
+
+console.log(pessoa.telefone);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
