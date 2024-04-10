0976dc286e8fffd059b3b67bc34aab55ac958344
Jacklin <132667129+JJacckkinninn@users.noreply.github.com>
2024-04-10 07:52:27 -0300

    Update copiando-arrays.js

--- copiando-arrays.js
+++ copiando-arrays.js
@@ -6,3 +6,20 @@ novasNotas.push(10);
 
 console.log(`As novas notas são ${novasNotas}`);
 console.log(`As notas originais são ${notas}`);
+
+
+const nomes = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João"];
+
+const meuSet = new Set(nomes);
+
+console.log(meuSet);
+
+
+
+const nomes = ["Ana", "Clara", "Maria", "Maria", "João", "João", "João"];
+
+const meuSet = new Set(nomes);
+
+const nomesAtualizados = [...meuSet];
+
+console.log(nomesAtualizados);
